e8588a9d8bbf3c5413e407614219e03332bdea52
Arjya Sen <114721138+virus3-0@users.noreply.github.com>
2022-10-10 23:52:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var total=0;
+for(var i=0; i<5; i++)
+
+{
+    var j = prompt("Please enter anything");
+    total=total+parseInt(j)
+    
+}
+document.write(total)    //print the output
+var text=""
